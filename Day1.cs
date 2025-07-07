8dd37cc0ef03b5c22e513205062468a2a6451f18
Harini112004 <harinia1110@gmail.com>
2025-07-07 14:02:28 +0530

    Add project files.

---
+++ Day1.cs
@@ -0,0 +1,91 @@
+﻿// WEEK 1 – DAY 1
+
+/**********************************************
+ * 1. SUM OF TWO NUMBERS FROM USER INPUT
+ **********************************************/
+
+/*
+using System;
+class Program
+{
+    static void Main(string[] args)
+     {
+         Console.WriteLine("Enter two numbers:");
+         int a = Convert.ToInt32(Console.ReadLine());
+         int b = Convert.ToInt32(Console.ReadLine());
+         Console.WriteLine("Sum is: " + (a + b));
+     }
+ }
+
+*/
+/**********************************************
+ * 2. AREA OF A CIRCLE
+ **********************************************/
+
+/*
+using System;
+
+namespace CircleApp
+ {
+class Program
+     {
+         static void Main(string[] args)
+         {
+             Console.Write("Enter the radius: ");
+             double r = Convert.ToDouble(Console.ReadLine());
+             double area = 3.14 * r * r;
+             Console.WriteLine("Area is: " + area);
+         }
+     }
+ }
+*/
+
+/**********************************************
+ * 3. CHECK IF NUMBER IS POSITIVE OR NEGATIVE
+ **********************************************/
+
+/*
+using System;
+
+namespace NumberCheck
+ {
+     class Program
+    {
+         static void Main(string[] args)
+         {
+             Console.Write("Enter a number: ");
+             int num = Convert.ToInt32(Console.ReadLine());
+             if (num > 0)
+                 Console.WriteLine(num + " is Positive");
+             else if (num < 0)
+                 Console.WriteLine(num + " is Negative");
+             else
+                 Console.WriteLine("It is Zero");
+         }
+     }
+ }
+*/
+
+/**********************************************
+ * 4. BASIC CALCULATOR (Add, Sub, Mul, Div)
+ **********************************************/
+
+/*
+using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter first number: ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Sum is: " + (a + b));
+        Console.WriteLine("Sub is: " + (a - b));
+        Console.WriteLine("Mul is: " + (a * b));
+        Console.WriteLine("Div is: " + (a / b));
+    }
+}
+*/
